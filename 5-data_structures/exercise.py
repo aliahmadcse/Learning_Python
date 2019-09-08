debfceacb3d4e75b6fa0b8dd4dcb2e638321db75
Ali Ahmad <aliahmadcse@gmail.com>
2019-09-08 15:37:41 +0500

    Modifies exercise file in data-structure folder

--- 5-data_structures/exercise.py
+++ 5-data_structures/exercise.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-
+import math
 sen = 'This is a common interview question'
 char_number = {}
 for item in sen:
@@ -12,3 +12,11 @@ sorted_dict = sorted(char_number.items(),
                      key=lambda item: item[1], reverse=True)
 
 pprint(sorted_dict[0],width=10)
+
+
+# def func(n):
+#     div = n/8
+#     return n-math.pow(2, div) > 0
+
+
+# print(func(1))
